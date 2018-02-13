dba6d65388ddafb22f7c798e4938743f605e44c9
James Smith <jrs43215@icloud.com>
2018-02-12 21:05:23 -0500

    BigIntegerPrimeNumberGenerator passes tests.

--- src/main/java/BigIntegerPrimeNumberGenerator.java
+++ src/main/java/BigIntegerPrimeNumberGenerator.java
@@ -1,13 +1,48 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BigIntegerPrimeNumberGenerator implements PrimeNumberGenerator {
+
+    private int certainty;
+
+    public BigIntegerPrimeNumberGenerator() {
+        this.certainty = 10;
+    }
+
+    public BigIntegerPrimeNumberGenerator(int certainty) {
+        this.certainty = certainty;
+    }
     @Override
     public List<Integer> generate(int startingValue, int endingValue) {
-        return null;
+        List<Integer> primes = new ArrayList<>();
+        int start, end;
+        if (startingValue < endingValue) {
+            start = startingValue;
+            end = endingValue;
+        } else {
+            start = endingValue;
+            end = startingValue;
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (this.isPrime(i)) {
+                primes.add(i);
+            }
+        }
+
+        return primes;
     }
 
     @Override
     public boolean isPrime(int value) {
-        return false;
+        return isPrime(BigInteger.valueOf(value));
+    }
+
+    private boolean isPrime(BigInteger value) {
+        if (value.compareTo(BigInteger.valueOf(2)) < 0) {
+            return false;
+        }
+        return value.isProbablePrime(this.certainty);
     }
 }
